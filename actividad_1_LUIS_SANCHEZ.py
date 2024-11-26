10ebb822e823e77c06fa6a46c54fea949b6702ac
Luis Alberto Sánchez <lasg125@gmail.com>
2024-11-26 23:41:26 +0100

    Adding documentation

--- actividad_1_LUIS_SANCHEZ.py
+++ actividad_1_LUIS_SANCHEZ.py
@@ -1,26 +1,11 @@
 """
-Se dispondrá de un menú que ofrecerá las siguientes opciones al usuario:
-[1] Introducir un nuevo jugador
-[2] Listar jugadores
-[3] Máximo anotador
-[4] Estadísticas del equipo
-[0] Salir del programa
-
-Requisitos mínimos
-Modularización: Es imprescindible generar al menos 3 funciones para resolver cualquiera de las problemáticas propuestas
-Uso de colecciones: Es imprescindible usar al menos un tipo de colecciones propuestas en el curso
-Comentarios: Es imprescindible comentar como mínimo todas las funciones definidas así como los bloques más importantes
-Buenas prácticas: Es imprescindible que el nombre de las variables y funciones siga unas buenas prácticas de definición
-"""
-
-"""
 Definición del jugador
 - Función Introducir un nuevo jugador
--- debe validar que el dorsal no esta repetido
+-- debe validar que el dorsal no está repetido
 -- la función debe recibir nombre, dorsal, canastas_3, canastas_2, canastas_1
 -- debo guardar los jugadores en una lista
 -- al guardar los valores sumar las canastas y guardar el total
--- las lista debe tener todos los datos de los jugadores.
+-- la lista debe tener todos los datos de los jugadores.
 
 - Función Listar los jugadores 
 -- obtener toda la lista
@@ -28,39 +13,19 @@ Definición del jugador
 -- en este listado las anotaciones deben ser totales por jugador
 
 - Función Máximo Anotador
--- Sumar los totales de las canastas de 3, 2 y 1 de cada jugador, devolver el jugador con el numero mayor
+-- Sumar los totales de las canastas de 3, 2 y 1 de cada jugador, devolver el jugador con el número mayor
 
 - Función Estadísticas del equipo
 -- Listar todos los jugadores con los valores de cada canasta y el total 
 
 - Función Salir del Programa
 """
+
 players = []
-
-
-def is_player_duplicated(dorsal):
-    """
-    Función que verifica si un jugador ya fue ingresado con ese mismo dorsal
-    :param dorsal: int number of the player
-    :return: True if player exist or False if not exist
-    """
-    result = False
-    if dorsal in players:
-        result = True
-    return result
-
-
-def is_string(text):
-    text = str(text)
-    return isinstance(text, str)
 
 
 def is_number(number):
     return number.isdigit()
-
-
-def is_empty(value):
-    return value == ''
 
 
 def sum_all_scores(canastas_1, canastas_2, canastas_3):
@@ -75,51 +40,100 @@ def sum_all_scores(canastas_1, canastas_2, canastas_3):
     return result
 
 
-def get_player(dorsal):
-    result = []
-    return players[dorsal]
-
-
 def create_nuevo_jugador(nombre, dorsal, canastas_1, canastas_2, canastas_3):
-    if is_player_duplicated(dorsal):
-        return False
-
-    players.append([nombre, dorsal, canastas_1, canastas_2, canastas_3])
+    """
+    Función que crea el nuevo jugador a portir de un set de parámetros
+    No se considera que llegue un valor vacío porque siempre se válida que tenga un input
+    :param nombre: String
+    :param dorsal: Int
+    :param canastas_1: Int
+    :param canastas_2: Int
+    :param canastas_3: Int
+    :return: Devuelve True luego de crearlo
+    """
+    # Suma los puntajes y lo agrega a la lista como total
+    total = sum_all_scores(canastas_1, canastas_2, canastas_3)
+    # Agrega los valores a la lista
+    players.append([nombre, dorsal, canastas_1, canastas_2, canastas_3, total])
     return True
 
 
-def get_lista_jugadores():
+def get_jugadores(estadistica_or_lista):
+    """
+    Función para listar la estadistica o la lista de jugadores, itera sobre la lista de acuerdo al parametro
+    :param estadistica_or_lista: recibe "estadistica" o "lista"
+    :return: cadena de texto con los valores de la lista seleccionada
+    """
     result = ""
+    # Si no hay jugadores en la lista le pide que cree uno
     if len(players) == 0:
         result = "\nNo hay jugadores, crea un jugador con la opción[1]\n"
     for index, player in enumerate(players):
-        result = f'\nJugador {index + 1}: Nombre {player[0]}, dorsal {player[1]}, canastas {player[2]}, canastas {player[3]}.\n'
-    return print(result)
-
-
-def get_maximo_anotador():
-    result = []
+        if estadistica_or_lista == "lista":
+            result += (f'Jugador {index + 1}: Nombre {player[0]} | '
+                       f'Dorsal: {player[1]} | '
+                       f'Total anotaciones: {player[-1]}.\n')
+        else:
+            result += (f'Jugador {index + 1}: Nombre {player[0]} | '
+                       f'Dorsal: {player[1]} | '
+                       f'Canasta 1: {player[2]} | '
+                       f'Canasta 2: {player[3]} | '
+                       f'Canasta 3: {player[4]} | '
+                       f'Total anotaciones: {player[-1]}.\n')
     return result
 
 
-def get_estadisticas_equipo():
-    result = []
+def get_max_anotador():
+    """
+    Función que haya el resultado máximo entre los jugadores agregados a la lista
+    El metodo max: recibe una lista a iterar y utiliza la función lambda. Compara usando el segundo elemento de cada sublista
+    # Hace internamente esto (ejemplo tomado de ChatGPT porque no conocía el método)
+    # jugadores = [["Juan", 10, 40], ["Ana", 15, 45], ["Pedro", 12, 42]]
+    # 1. Para ["Juan", 10, 40] → devuelve 40
+    # 2. Para ["Ana", 15, 45] → devuelve 45
+    # 3. Para ["Pedro", 12, 42] → devuelve 42
+    # 4. Compara: 40 vs 45 vs 42
+    # 5. Identifica que 45 es el mayor
+    # 6. Devuelve la lista completa ["Ana", 15, 45]
+    :return: String con el jugador y el número de anotaciones total
+    """
+    result = ""
+    if len(players) > 0:
+        result = max(players, key=lambda x: x[-1])
+        result = f"Jugador: {result[0]}, Valor máximo: {result[-1]}"
+    else:
+        result = "No hay jugadores en la lista, agrega uno"
     return result
 
 
 def salir_programa():
-    result = print("\nAdios, Gracias!!\n")
-    return result
+    """
+    Función para mostrar el texto de salida del sistema
+    :return: Texto de salida
+    """
+    return print("\nAdios, Gracias!!\n")
 
 
 def get_player_numeric_input(player_attribute):
+    """
+    Función para validar si el valor que el usuario ingresa es numérico, si no itera hasta que ingrese un input válido
+    :param player_attribute: Recibe el atributo del jugador que vamos a ingresar: dorsal, nombre, etc.
+    :return: El input del usuario ingresado
+    """
     user_input = input(f"Escribe el {player_attribute} del jugador: ")
+
     while not is_number(user_input):
         user_input = input(f"El {player_attribute} debe ser un numero: ")
     return user_input
 
 
 def initialize_game():
+    """
+    Función principal de inicialización y control de las interacciones
+    Muestra el menú principal disponible para el usuario
+    De acuerdo al menú seleccionado el sistema llama a las diferentes funciones del programa
+    """
+
     print("Hola, selecciona una opción usando el teclado:"
           "\n[1] Introducir un nuevo jugador",
           "\n[2] Listar jugadores",
@@ -127,11 +141,11 @@ def initialize_game():
           "\n[4] Estadísticas del equipo",
           "\n[0] Salir del programa")
 
-    user_selection = input("Escribe un número de 0 a 4: ")
+    user_selection = input("\nEscribe un número de 0 a 4: ")
 
     if user_selection == "1":
 
-        player_name = input("Escribe el nombre del jugador: ")
+        player_name = input("\nEscribe el nombre del jugador: ")
         player_number = get_player_numeric_input("dorsal")
         player_score_1 = get_player_numeric_input("canastas 1")
         player_score_2 = get_player_numeric_input("canastas 2")
@@ -145,20 +159,27 @@ def initialize_game():
             int(player_score_3)
         )
         if new_player:
-            print(f"Jugador creado exitosamente {players[-1]}\n")
+            print("\n==============Creación jugador============")
+            print(f"Jugador creado exitosamente {players[-1]}")
+            print("==============Fin Creación de Jugadores============\n")
             initialize_game()
 
     elif user_selection == "2":
-        print("Lista de Jugadores")
-        get_lista_jugadores()
+        print("\n==============Lista de Jugadores============")
+        print(get_jugadores("lista"))
+        print("==============Fin lista de Jugadores============\n")
         initialize_game()
 
     elif user_selection == "3":
-        get_maximo_anotador()
+        print("\n==============Máximo anotador del equipo============")
+        print(get_max_anotador())
+        print("==============Fin anotador del equipo===========\n")
         initialize_game()
 
     elif user_selection == "4":
-        get_estadisticas_equipo()
+        print("\n==============Estadísticas de Jugadores============")
+        print(get_jugadores("estadistica"))
+        print("==============Fin Estadísticas de Jugadores============\n")
         initialize_game()
 
     elif user_selection == "0":
@@ -166,7 +187,7 @@ def initialize_game():
     else:
         print("Seleccionaste una opción que no existe. Intenta de nuevo\n")
         initialize_game()
-    return user_selection
 
 
+# Inicializa la aplicación
 initialize_game()
