39bc67c5d808bf414e0645f38966cc4175ae4489
Luis Alberto Sánchez <lasg125@gmail.com>
2024-12-09 21:28:44 +0100

    adding tests with pytest

--- IS101/cifrado_cesar/test_cifrador_cesar.py
+++ IS101/cifrado_cesar/test_cifrador_cesar.py
@@ -1,10 +1,16 @@
 import cifrador_cesar
 
-cesar_3 = cifrador_cesar.new_encryptor(3)
-assert cesar_3('ZigZag') == "BLJBDJ"
 
-cesar_2 = cifrador_cesar.new_encryptor(2)
-assert cesar_2('ZigZag') == "AKIACI"
+def test_cifrado_simple_3():
+    cesar_3 = cifrador_cesar.new_encryptor(3)
+    assert cesar_3('ZigZag') == "BLJBDJ"
 
-reverse_2 = cifrador_cesar.new_encryptor(-2)
-assert reverse_2('akiaci') == "ZIGZAG"
+
+def test_cifrado_simple_2():
+    cesar_2 = cifrador_cesar.new_encryptor(2)
+    assert cesar_2('ZigZag') == "AKIACI"
+
+
+def test_cifrado_reverso():
+    reverse_2 = cifrador_cesar.new_encryptor(-2)
+    assert reverse_2('akiaci') == "ZIGZAG"
